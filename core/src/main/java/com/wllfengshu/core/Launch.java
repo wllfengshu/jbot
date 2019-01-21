1f0f15067673a830e8f0a9e1f1887df1d5599fef
snl <sunl@welljoint.com>
2019-01-21 11:59:50 +0800

    增加多线程（newFixedThreadPool+Future）

--- core/src/main/java/com/wllfengshu/core/Launch.java
+++ core/src/main/java/com/wllfengshu/core/Launch.java
@@ -15,35 +15,61 @@ import com.wllfengshu.core.work.resourcesHandle.PropertiesHandle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 /**
  * 把model工程按照需求进行修改
  */
 public class Launch {
 
     private static Logger logger = LoggerFactory.getLogger(Launch.class);
+    private static List<Future<Boolean>> futures = new ArrayList<>();
+    private static ExecutorService executorService = Executors.newFixedThreadPool(3);
+
 
     public static boolean start(String projectName, String packageName, DBInfo dbInfo){
         logger.info("jbot core,Launch,start-------->dbInfo:%s,projectName:%s,packageName:%s",dbInfo,projectName,packageName);
-        //1、准备工作
-        RequestModel model = BeforeHandle.start(projectName, packageName, dbInfo);
-        //2、配置文件修改
-        DockerfileHandle.start(model);
-        PomHandle.start(model);
-        ReadmeHandle.start(model);
-        StartupHandle.start(model);
-        //3、java文件修改
-        DaoHandle.start(model);
-        ServiceHandle.start(model);
-        ServiceImplHandle.start(model);
-        RestHandle.start(model);
-        EntityHandle.start(model);
-        ApplicationHandle.start(model);
-        //4、resources文件修改
-        LogbackHandle.start(model);
-        MapperHandle.start(model);
-        PropertiesHandle.start(model);
-        //5、善后工作
-        AfterHandle.start(model);
-        return true;
+        try {
+            //1、准备工作
+            RequestModel model = BeforeHandle.start(projectName, packageName, dbInfo);
+            futures.add(executorService.submit(() -> {
+                //2、配置文件修改
+                DockerfileHandle.start(model);
+                PomHandle.start(model);
+                ReadmeHandle.start(model);
+                StartupHandle.start(model);
+                return true;
+            }));
+            futures.add(executorService.submit(() -> {
+                //3、java文件修改
+                DaoHandle.start(model);
+                ServiceHandle.start(model);
+                ServiceImplHandle.start(model);
+                RestHandle.start(model);
+                EntityHandle.start(model);
+                ApplicationHandle.start(model);
+                return true;
+            }));
+            futures.add(executorService.submit(() -> {
+                //4、resources文件修改
+                LogbackHandle.start(model);
+                MapperHandle.start(model);
+                PropertiesHandle.start(model);
+                return true;
+            }));
+            for (Future<Boolean> future : futures) {
+                if (!future.isDone()){
+                    future.get();
+                }
+            }
+            //5、善后工作
+            AfterHandle.start(model);
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
     }
 }
